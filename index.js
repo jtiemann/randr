c62135195f3c5bdb4bddc50dec3ce324283cc67b
Jon Tiemann <jtiemann@digitalpersonae.com>
2017-03-07 15:37:02 -0500

    match/create on lname, posts to db with relationship!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const R = require('ramda')
 const throttle = require('lodash.throttle');
 var myLatLng=[0,0]
+
 const apiCall = urlStr => dataObj => {
 	return axios({
 	  method: 'post',
@@ -14,35 +15,9 @@ const theThen = prom => prom.then(resp => {
   	document.querySelector('#raves').innerHTML=""
   	document.querySelector('#rants').innerHTML=""
     theMap.removeMarkers()
-
-    resp.data.map((data,i) => {
-	    theMap.addMarker({
-			  lat: data[1].latlng[0] + i*.0003,
-			  lng: data[1].latlng[1] + i*.0003,
-			  title: data[1].text,
-			  mouseout: function(e) { theMap.hideInfoWindows() },
-			  mouseover: function(e){ this.infoWindow.open(this.map, this) },
-			  infoWindow: {content: `<p>${data[1].text}</p>`},
-			  id: i
-        })
-    // render randr's
-    if (data[1].r == "Rant") document.querySelector('#rants').innerHTML = document.querySelector('#rants').innerHTML 
-    	+ `<div data-iw="${i}" class="rant">
-                <div class="rText">${data[1].text}</div> <div class="rName"> ${data[0].fname + " " + data[0].lname}</div> <div class="rDate">${(new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()}</div>
-           </div>`
-    else document.querySelector('#raves').innerHTML = document.querySelector('#raves').innerHTML 
-    	+ `<div data-iw="${i}" class="rave">
-                <div class="rText">${data[1].text}</div> <div class="rName"> ${data[0].fname + " " + data[0].lname}</div> <div class="rDate">${(new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()}</div>
-           </div>`
-    });
-    // add events on text randrs to open infoWindow
-    [].slice.call(document.querySelectorAll('[data-iw]'))
-        .map(r=>r.addEventListener('mouseenter', 
-		 	(e) => {
-		 		theMap.hideInfoWindows()
-		 		let jim = theMap.markers.filter(x=>x.id === parseInt(e.target.dataset.iw))[0]
-		 		jim.infoWindow.open(theMap, jim)
-		    }))
+    doRenderMarkers(resp)
+    doRenderRantsAndRaves(resp)
+    doAddeventListeners()
   	console.log(resp)
   })
 
@@ -70,29 +45,77 @@ const getLocation = () => {
     } 
     else x.innerHTML = "Geolocation is not supported by this browser."}
 
+const doRenderMarkers = function(idata, single=false){
+	if (single == true) {
+		 var h = new Array()
+		 var t =  h.slice.call(document.querySelectorAll('[data-iw]')).map(r=>parseInt(r.dataset.iw))
+         var idx = Math.max(...t) + 1
+	}
+	    idata.data.map((data,i) => {
+		    theMap.addMarker({
+				  lat: single ? userLatLng.value.split(",")[0] : data[1].latlng[0] + i*.0003,
+				  lng: single ? userLatLng.value.split(",")[1] : data[1].latlng[1] + i*.0003,
+				  title: single ? textR.value :  data[1].text,
+				  mouseout: function(e) { theMap.hideInfoWindows() },
+				  mouseover: function(e){ this.infoWindow.open(this.map, this) },
+				  infoWindow: {content: `<p>${single ? textR.value :  data[1].text}</p>`},
+				  id: single ? idx : i
+	        })
+     	})
+     	return idata
+	}
+
+const doRenderRantsAndRaves = function(idata, single=false) {
+	if (single == true) {
+		 var h = new Array()
+         var idx = Math.max(... h.slice.call(document.querySelectorAll('[data-iw]')).map(r=>parseInt(r.dataset.iw))) + 1
+    }
+    idata.data.map((data,i) => {
+		if (single  && document.querySelector('input[name="rantOrRave"]:checked').value == "imARant" || !single && data[1].r == "Rant") 
+			document.querySelector('#rants').innerHTML = document.querySelector('#rants').innerHTML 
+	    	+ `<div data-iw="${single ? idx : i}" class="rant"> 
+	                <div class="rText">${single ? textR.value : data[1].text}</div> 
+	                <div class="rName"> ${ single ? rOrRSignature.value : data[0].lname}</div> 
+	                <div class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</div>
+	           </div>`
+
+        else 
+        	document.querySelector('#raves').innerHTML = document.querySelector('#raves').innerHTML 
+	    	+ `<div data-iw="${single ? idx : i}" class="rave">
+	                <div class="rText">${single ? textR.value : data[1].text }</div> 
+	                <div class="rName"> ${  single ? rOrRSignature.value : data[0].lname}</div> 
+	                <div class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</div>
+	           </div>`
+     	})
+     	return idata	
+}
+const doAddeventListeners = function(idata){
+	var h = new Array()
+    var openMarkerText = (e) => {
+		 		theMap.hideInfoWindows()
+		 		let jim = theMap.markers.filter(x=>x.id === parseInt(e.target.dataset.iw))[0]
+		 		jim.infoWindow.open(theMap, jim)
+		    }
+
+    h.slice.call(document.querySelectorAll('[data-iw]'))
+        .map(r=> { r.removeEventListener('mouseenter', openMarkerText); return r})
+        .map(r=> { r.addEventListener('mouseenter', openMarkerText); return r})
+
+        return idata
+}
+
 getLocation();
 
-// Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("addR");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
 	document.querySelector("#userLatLng").value = myLatLng
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -101,43 +124,15 @@ window.onclick = function(event) {
 
 document.getElementById('addRantOrRaveForm').addEventListener('submit',  evt => {
 	evt.preventDefault()
-    console.log(evt)
 	console.log(userLatLng.value.split(","), 
 				document.querySelector('input[name="rantOrRave"]:checked').value, 
 				textR.value, 
 				rOrRSignature.value)
 
-    
-
-	    theMap.addMarker({
-			  lat: userLatLng.value.split(",")[0],
-			  lng: userLatLng.value.split(",")[1],
-			  title: textR.value,
-			  mouseout: function(e) { theMap.hideInfoWindows() },
-			  mouseover: function(e){ this.infoWindow.open(this.map, this) },
-			  infoWindow: {content: `<p>${textR.value}</p>`},
-			  id:1000
-        })
-    // render randr's
-    if (document.querySelector('input[name="rantOrRave"]:checked').value == "imARant") document.querySelector('#rants').innerHTML = document.querySelector('#rants').innerHTML 
-    	+ `<div data-iw="1000" class="rant"> 
-                <div class="rText">${textR.value}</div> <div class="rName"> ${ rOrRSignature.value}</div> <div class="rDate">${(new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString()}</div>
-           </div>`
-    else document.querySelector('#raves').innerHTML = document.querySelector('#raves').innerHTML 
-    	+ `<div data-iw="1000" class="rave">
-                <div class="rText">${textR.value}</div> <div class="rName"> ${ rOrRSignature.value}</div> <div class="rDate">${(new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString()}</div>
-           </div>`
-
-    // add events on text randrs to open infoWindow
-    var h = new Array()
-    h.slice.call(document.querySelectorAll('[data-iw]'))
-        .map(r=>r.addEventListener('mouseenter', 
-		 	(e) => {
-		 		theMap.hideInfoWindows()
-		 		let jim = theMap.markers.filter(x=>x.id === parseInt(e.target.dataset.iw))[0]
-		 		jim.infoWindow.open(theMap, jim)
-		    }))
-  	console.log('hi')
+    apiCall('/postR')({type: document.querySelector('input[name="rantOrRave"]:checked').value == 'imARant' ? 'rant' : 'rave', text: textR.value, lname: rOrRSignature.value, latlng:userLatLng.value.split(",")})
+    doRenderMarkers({data: ["empty"]}, true)
+    doRenderRantsAndRaves({data: ["empty"]}, true)
+    doAddeventListeners()
 
     modal.style.display = "none";
     
--- server.js
+++ server.js
@@ -31,21 +31,24 @@ server.get('\/node_modules\/gmaps\/?.*', restify.serveStatic({
 }));
 
 server.post('/postR', function create(req, res) {
-    console.log('lo: ', req.body)
+    console.log('posted: ', req.body)
 
-    var qa = `merge (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rave) where m.latlng[0]<${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
-              and  m.latlng[1]<${req.body.nelng} and m.latlng[1]>${req.body.swlng} 
-              return p, m {.text, r:'Rave', .latlng, .timedate} UNION 
-          match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rant) where m.latlng[0]< ${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
-              and  m.latlng[1]<${req.body.nelng} and m.latlng[1]>${req.body.swlng}
-              return p, m {.text, r:'Rant', .latlng, .timedate}  `
+    var qa = `merge (p:Person {lname:'${req.body.lname}'})  
+    create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp() })-[:IS_A]->(:Rant) `
 
-    db.cypherQuery(qa, 
+    var qb = `merge (p:Person {lname:'${req.body.lname}'})  
+    create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp()  })-[:IS_A]->(:Rave) `
+    
+
+    var query = req.body.type=='rant' ? qa : qb
+        console.log('--hiu'); // delivers an array of names of objects getting returned
+
+    db.cypherQuery(query, 
         function(err, result){
             if(err) throw err;
 
             console.log(result.data); // delivers an array of query results
-            console.log(result.columns); // delivers an array of names of objects getting returned
+            console.log('--hi'); // delivers an array of names of objects getting returned
             res.send(result.data)
     });     
 });
@@ -71,8 +74,8 @@ var qa = `match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rave) where m.la
         function(err, result){
             if(err) throw err;
 
-            console.log(result.data); // delivers an array of query results
-            console.log(result.columns); // delivers an array of names of objects getting returned
+            //console.log(result.data); // delivers an array of query results
+            //console.log(result.columns); // delivers an array of names of objects getting returned
             res.send(result.data)
     });     
 });
