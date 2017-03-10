e977f323607930a52c102c775122d538bc1e0cda
Jon Tiemann <jtiemann@digitalpersonae.com>
2017-03-10 17:35:05 -0500

    groups (by where) rants and raves and concats text

--- dist/index.js
+++ dist/index.js
@@ -208,6 +208,19 @@ const doClearMap = function(){
   	document.querySelector('#rants').innerHTML=""
     theMap.removeMarkers()
 }
+const preprocessGroupedData = function(data) {
+        if (data.length > 1) {
+        	data = data.reduce((sum, unit)=>{
+                 sum.length == 0 ? sum = unit : sum[1].text = sum[1].text + "; " + unit[1].text
+                 	//sum = R.assocPath([1,'text'], R.path([1,'text'], sum) + "; " + R.path([1,'text'], unit)) (sum)
+                 return sum
+            }, [])
+        }
+        else {
+        	data = R.flatten(data)
+        }	
+        return data
+}
 const doRenderMarkers = function(idata, single=false){
 
 	if (single == true) {
@@ -215,14 +228,18 @@ const doRenderMarkers = function(idata, single=false){
 		 var t =  h.slice.call(document.querySelectorAll('[data-iw]')).map(r=>parseInt(r.dataset.iw))
          var idx = Math.max(...t) + 1
 	}
-	    idata.data.map((data,i) => {
-/*	    	//Leaflet-MapBox Map, display only, no events yet.
-            L.marker([single ? userLatLng.value.split(",")[0] : data[1].latlng[0], single ? userLatLng.value.split(",")[1] : data[1].latlng[1]  ]).addTo(mymap).bindPopup(single ? textR.value :  data[1].text)
-*/
+	const s = R.sortWith([R.ascend(R.compose(R.toLower, R.path([2, "where"]))), R.ascend(R.path([1,'r'])), R.descend(R.path([1, "timedate"]))])
+	const g = R.groupWith((a,b) => R.path([1, 'r'], a) == R.path([1, 'r'], b) && R.path([2, 'where'], a) == R.path([2, 'where'], b), R.clone(s(idata.data)))
+	console.log('grouped: ', g)
+	    g.map((data,i) => {
+            data = preprocessGroupedData(data)	
+		    	//Leaflet-MapBox Map, display only, no events yet.
+	            //L.marker([single ? userLatLng.value.split(",")[0] : data[1].latlng[0], single ? userLatLng.value.split(",")[1] : data[1].latlng[1]  ]).addTo(mymap).bindPopup(single ? textR.value :  data[1].text)
+	
             //gmap Map
 		    theMap.addMarker({
-				  lat: single ? userLatLng.value.split(",")[0] : data[1].latlng[0] + i*.0003,
-				  lng: single ? userLatLng.value.split(",")[1] : data[1].latlng[1] + i*.0003,
+				  lat: single ? userLatLng.value.split(",")[0] : data[1].latlng[0] ,
+				  lng: single ? userLatLng.value.split(",")[1] : data[1].latlng[1] ,
 				  title: single ? textR.value :  data[1].text,
 				  mouseout: function(e) { theMap.hideInfoWindows() },
 				  mouseover: function(e){ this.infoWindow.open(this.map, this) },
@@ -238,21 +255,34 @@ const doRenderRantsAndRaves = function(idata, single=false) {
 		 var h = new Array()
          var idx = Math.max(... h.slice.call(document.querySelectorAll('[data-iw]')).map(r=>parseInt(r.dataset.iw))) + 1
     }
-    idata.data.map((data,i) => {
+
+	const s = R.sortWith([R.ascend(R.compose(R.toLower, R.path([2, "where"]))), R.ascend(R.path([1,'r'])), R.descend(R.path([1, "timedate"]))])
+	const g = R.groupWith((a,b) => R.path([1, 'r'], a) == R.path([1, 'r'], b) && R.path([2, 'where'], a) == R.path([2, 'where'], b), R.clone(s(idata.data)))
+	console.log('grouped: ', g)
+	    g.map((data,i) => {
+            data = preprocessGroupedData(data)	
+
+        // render a rant or rave
 		if (single  && document.querySelector('input[name="rantOrRave"]:checked').value == "imARant" || !single && data[1].r == "Rant") 
 			document.querySelector('#rants').innerHTML = document.querySelector('#rants').innerHTML 
 	    	+ `<div data-iw="${single ? idx : i}" class="rant"> 
+	    		    <div class="where">${single ? where.value : data[2].where}</div> 
 	                <div class="rText">${single ? textR.value : data[1].text}</div> 
-	                <div class="rName"> ${ single ? rOrRSignature.value : data[0].lname}</div> 
-	                <div class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</div>
+	                <div> 
+	                  <span class="rName"> ${ single ? rOrRSignature.value : data[0].lname} </span>
+	                  <span class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</span>
+	                </div>
 	           </div>`
 
         else 
         	document.querySelector('#raves').innerHTML = document.querySelector('#raves').innerHTML 
 	    	+ `<div data-iw="${single ? idx : i}" class="rave">
+	    		    <div class="where">${single ? where.value : data[2].where}</div> 
 	                <div class="rText">${single ? textR.value : data[1].text }</div> 
-	                <div class="rName"> ${  single ? rOrRSignature.value : data[0].lname}</div> 
-	                <div class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</div>
+	                <div> 
+	                  <span class="rName"> ${ single ? rOrRSignature.value : data[0].lname} </span>
+	                  <span class="rDate">${single ? (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString() : (new Date(data[1].timedate)).toLocaleDateString() + " " + (new Date(data[1].timedate)).toLocaleTimeString()   }</span>
+	                </div>
 	           </div>`
      	})
      	return idata	
@@ -282,7 +312,7 @@ const getData = map => {
 
 const drawMap = position => {
     myLatLng = [position.coords.latitude, position.coords.longitude]
-/*     //Leaflet-MapBox Map
+	/*     //Leaflet-MapBox Map
     mymap = L.map('mapid').setView(myLatLng, 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianRpZW1hbm4iLCJhIjoiY2owMGRxNjByMDIxejMzbXlzeDFxNnBkdyJ9.h6F_Md4FOBrOB7yRE_KoLA', {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -296,10 +326,9 @@ const drawMap = position => {
 	lat: position.coords.latitude,
 	lng: position.coords.longitude,
 	bounds_changed: throttle(R.compose(theThen, apiCall('/getDataByCoords'), getData), 1000)
-	});}
+	})}
 
 var x = document.getElementById("map");
-
 const getLocation = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(R.compose(drawMap));
@@ -328,10 +357,15 @@ document.getElementById('addRantOrRaveForm').addEventListener('submit',  evt => 
 	evt.preventDefault()
 	console.log(userLatLng.value.split(","), 
 				document.querySelector('input[name="rantOrRave"]:checked').value, 
+				where.value,
 				textR.value, 
 				rOrRSignature.value)
 
-    apiCall('/postR')({type: document.querySelector('input[name="rantOrRave"]:checked').value == 'imARant' ? 'rant' : 'rave', text: textR.value, lname: rOrRSignature.value, latlng:userLatLng.value.split(",")})
+    apiCall('/postR')({type: document.querySelector('input[name="rantOrRave"]:checked').value == 'imARant' ? 'rant' : 'rave', 
+    	               where: where.value, 
+    	               text: textR.value, 
+    	               lname: rOrRSignature.value, 
+    	               latlng:userLatLng.value.split(",")})
     doRenderMarkers({data: ["empty"]}, true)
     doRenderRantsAndRaves({data: ["empty"]}, true)
     doAddeventListeners()
--- server.js
+++ server.js
@@ -19,11 +19,13 @@ server.get('\/node_modules\/gmaps\/?.*', restify.serveStatic({
 server.post('/postR', function create(req, res) {
     console.log('posted: ', req.body)
 
-    var qa = `merge (p:Person {lname:'${req.body.lname}'})  
-    create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp() })-[:IS_A]->(:Rant) `
+    var qa = `merge (p:Person {lname:'${req.body.lname}'})  merge (w:Where {where:'${req.body.where}'})
+                create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp() })-[:IS_A]->(:Rant) 
+                create (m)-[:LOCATED]->(w) `
 
-    var qb = `merge (p:Person {lname:'${req.body.lname}'})  
-    create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp()  })-[:IS_A]->(:Rave) `
+    var qb = `merge (p:Person {lname:'${req.body.lname}'})  merge (w:Where {where:'${req.body.where}'})
+                create (p)-[l:AUTHORED]->(m:Message {text:'${req.body.text}', latlng:[${req.body.latlng}], timedate: timestamp()  })-[:IS_A]->(:Rave) 
+                create (m)-[:LOCATED]->(w) `
     
 
     var query = req.body.type=='rant' ? qa : qb
@@ -39,12 +41,12 @@ server.post('/postR', function create(req, res) {
 server.post('/getDataByCoords', function create(req, res) {
     console.log('lo: ', req.body)
 
-    var qa = `match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rave) where m.latlng[0]<${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
-              and  m.latlng[1]<${req.body.nelng} and m.latlng[1]>${req.body.swlng} 
-              return p, m {.text, r:'Rave', .latlng, .timedate} UNION 
-          match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rant) where m.latlng[0]< ${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
+    var qa = `match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rave), (m)-[:LOCATED]->(w:Where) where m.latlng[0]<${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
               and  m.latlng[1]<${req.body.nelng} and m.latlng[1]>${req.body.swlng}
-              return p, m {.text, r:'Rant', .latlng, .timedate}  `
+              return p, m {.text, r:'Rave', .latlng, .timedate}, w UNION 
+          match (p:Person)-[l:AUTHORED]->(m:Message)-[:IS_A]->(:Rant), (m)-[:LOCATED]->(w:Where) where m.latlng[0]< ${req.body.nelat} and m.latlng[0]>${req.body.swlat} 
+              and  m.latlng[1]<${req.body.nelng} and m.latlng[1]>${req.body.swlng}
+              return p, m {.text, r:'Rant', .latlng, .timedate}, w  `
 
     db.cypherQuery(qa, 
         function(err, result){
